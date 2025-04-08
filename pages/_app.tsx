dc5adc9fe1f522458619f9f0c5292981a32ac788
rimurukiyowo <arakiyomi71@gmail.com>
2025-04-08 02:51:05 +0000

    update from replit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+//import '../styles/globals.css'
+//import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+//function MyApp({ Component, pageProps }: //AppProps) {
+  //return <Component {...pageProps} />
+//}
+
+//export default MyApp
+import Head from 'next/head';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/generated-icon.png" />
+        <meta name="theme-color" content="#000000" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
