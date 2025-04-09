7406dcf4c46a2c333597e40cb8cf07d8d2d4c9ba
rimurukiyowo <arakiyomi71@gmail.com>
2025-04-09 21:05:30 +0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+const myArray = (process.env.SOME_ENV_VAR || '').split(',');
+
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    MY_ARRAY: myArray.join(','),
+  },
+};
+
+module.exports = nextConfig;
