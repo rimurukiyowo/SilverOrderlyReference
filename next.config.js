300496016318ec627b059c86efcb9d65b33f57f0
rimurukiyowo <arakiyomi71@gmail.com>
2025-04-11 22:02:50 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
-const myArray = (process.env.SOME_ENV_VAR || '').split(',');
-
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    MY_ARRAY: myArray.join(','),
-  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
